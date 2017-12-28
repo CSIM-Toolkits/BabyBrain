5c14f9ccd0cb413d8522cec1574463ad65ec0bd8
Antonio Carlos Senra Filho <acsenrafilho@gmail.com>
2017-12-28 10:04:48 -0200

    ENH: Added skeleton of BabyBrainPreparation module
    A simple list of functions and parameters were added. However, no logical programming was inserted.

--- BabyBrainPreparation/BabyBrainPreparation.py
+++ BabyBrainPreparation/BabyBrainPreparation.py
@@ -15,18 +15,16 @@ class BabyBrainPreparation(ScriptedLoadableModule):
 
   def __init__(self, parent):
     ScriptedLoadableModule.__init__(self, parent)
-    self.parent.title = "BabyBrainPreparation" # TODO make this more human readable by adding spaces
+    self.parent.title = "Baby Brain Preparation" # TODO make this more human readable by adding spaces
     self.parent.categories = ["Examples"]
     self.parent.dependencies = []
-    self.parent.contributors = ["John Doe (AnyWare Corp.)"] # replace with "Firstname Lastname (Organization)"
+    self.parent.contributors = ["Antonio Carlos da S. Senra Filho (University of Sao Paulo) and Sara"] # replace with "Firstname Lastname (Organization)"
     self.parent.helpText = """
-This is an example of scripted loadable module bundled in an extension.
-It performs a simple thresholding on the input volume and optionally captures a screenshot.
+This module offers a set of algorithms to brain tissue preparation ... in structural MRI images, namely T1, T2 and PD. This module is the main function to ..., being at moment the naive Bayes classifier available. More details are found in the wikipage: https://www.slicer.org/wiki/Documentation/Nightly/Modules/BabyBrainPreparation
 """
     self.parent.helpText += self.getDefaultModuleDocumentationLink()
     self.parent.acknowledgementText = """
-This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc.
-and Steve Pieper, Isomics, Inc. and was partially funded by NIH grant 3P41RR013218-12S1.
+This work was partially funded by CNPq grant ....
 """ # replace with organization, grant and thanks.
 
 #
@@ -60,28 +58,248 @@ class BabyBrainPreparationWidget(ScriptedLoadableModuleWidget):
     self.inputSelector.nodeTypes = ["vtkMRMLScalarVolumeNode"]
     self.inputSelector.selectNodeUponCreation = True
     self.inputSelector.addEnabled = False
-    self.inputSelector.removeEnabled = False
+    self.inputSelector.removeEnabled = True
     self.inputSelector.noneEnabled = False
     self.inputSelector.showHidden = False
     self.inputSelector.showChildNodeTypes = False
     self.inputSelector.setMRMLScene( slicer.mrmlScene )
-    self.inputSelector.setToolTip( "Pick the input to the algorithm." )
+    self.inputSelector.setToolTip( "Pick the input to the algorithm. This should be an MRI strutural images with a type listed in the Image Modality option." )
     parametersFormLayout.addRow("Input Volume: ", self.inputSelector)
 
     #
-    # output volume selector
+    # Image Modality
+    #
+    self.setImageModalityBooleanWidget = ctk.ctkComboBox()
+    self.setImageModalityBooleanWidget.addItem("T1")
+    self.setImageModalityBooleanWidget.addItem("T2")
+    self.setImageModalityBooleanWidget.addItem("PD")
+    self.setImageModalityBooleanWidget.setToolTip(
+      "MRI strutural image inserted as a input volume.")
+    parametersFormLayout.addRow("Image Modality ", self.setImageModalityBooleanWidget)
+
+
+    #
+    # output volume selector TODO Add rename option 
     #
     self.outputSelector = slicer.qMRMLNodeComboBox()
     self.outputSelector.nodeTypes = ["vtkMRMLScalarVolumeNode"]
     self.outputSelector.selectNodeUponCreation = True
-    self.outputSelector.addEnabled = True
+    self.outputSelector.addEnabled = True 
     self.outputSelector.removeEnabled = True
-    self.outputSelector.noneEnabled = True
+    self.outputSelector.noneEnabled = False
     self.outputSelector.showHidden = False
     self.outputSelector.showChildNodeTypes = False
     self.outputSelector.setMRMLScene( slicer.mrmlScene )
     self.outputSelector.setToolTip( "Pick the output to the algorithm." )
     parametersFormLayout.addRow("Output Volume: ", self.outputSelector)
+
+    #
+    # Enhancement Functions Area
+    #
+    parametersEnhancementCollapsibleButton = ctk.ctkCollapsibleButton()
+    parametersEnhancementCollapsibleButton.text = "Image Enhancement Functions"
+    self.layout.addWidget(parametersEnhancementCollapsibleButton)
+
+    # Layout within the dummy collapsible button
+    parametersEnhancementLayout = qt.QFormLayout(parametersEnhancementCollapsibleButton)
+
+    #
+    # Apply Image Space Resampling
+    #
+    self.setImageResamplingBooleanWidget = ctk.ctkCheckBox()
+    self.setImageResamplingBooleanWidget.setChecked(True)
+    self.setImageResamplingBooleanWidget.setToolTip(
+      "Apply an image space resampling in the input image in order to reduce the total processing time. This is helpful for image with large scale space (usually higher than 256^3 mm).")
+    parametersEnhancementLayout.addRow("Apply Image Space Resampling", self.setImageResamplingBooleanWidget)
+
+    #
+    # Apply AAD filtering
+    #
+    self.setApplyAADBooleanWidget = ctk.ctkCheckBox()
+    self.setApplyAADBooleanWidget.setChecked(True)
+    self.setApplyAADBooleanWidget.setToolTip(
+      "Apply the AAD filter on the input data. This is recommended because the image noise level may affect the segmentation performance.")
+    parametersEnhancementLayout.addRow("Apply AAD filter", self.setApplyAADBooleanWidget)
+
+
+    #
+    # Apply Bias Correction
+    #
+    self.setApplyBiasCorrectionBooleanWidget = ctk.ctkCheckBox()
+    self.setApplyBiasCorrectionBooleanWidget.setChecked(True)
+    self.setApplyBiasCorrectionBooleanWidget.setToolTip(
+      "Apply a bias field correction in the input data. This is recommended because the global signal fluctuation provided by magnetic field inhomogeneity may affect the segmentation performance.")
+    parametersEnhancementLayout.addRow("Apply Bias Field Correction", self.setApplyBiasCorrectionBooleanWidget)
+
+    #
+    # Apply Global Contrast Enhancement Function
+    #
+    self.setApplyGlobalEnhancementBooleanWidget = ctk.ctkCheckBox()
+    self.setApplyGlobalEnhancementBooleanWidget.setChecked(True)
+    self.setApplyGlobalEnhancementBooleanWidget.setToolTip(
+      "Apply a full image space contrast enhancement in order to increase tissues delineation. This is recommended because the original image may present a poor separation between brain tissues.")
+    parametersEnhancementLayout.addRow("Apply Global Contrast Enhancement Function", self.setApplyGlobalEnhancementBooleanWidget)
+
+
+    #
+    # Image Space Resampling Parameters Area
+    #
+    parametersImageResamplingCollapsibleButton = ctk.ctkCollapsibleButton()
+    parametersImageResamplingCollapsibleButton.text = "Image Space Resampling Parameters"
+    self.layout.addWidget(parametersImageResamplingCollapsibleButton)
+
+    # Layout within the dummy collapsible button
+    parametersImageResamplingLayout = qt.QFormLayout(parametersImageResamplingCollapsibleButton)
+
+    # TODO Colocar uma entrada vector float para o ResampleScalarVolume CLI module -- similar ao N4ITK
+
+    #
+    # Interpolation Functions
+    #
+    self.setInterpolationFunctionBooleanWidget = ctk.ctkComboBox()
+    self.setInterpolationFunctionBooleanWidget.addItem("bspline")
+    self.setInterpolationFunctionBooleanWidget.addItem("linear")
+    self.setInterpolationFunctionBooleanWidget.addItem("nearestNeighbor")
+    self.setInterpolationFunctionBooleanWidget.addItem("hamming")
+    self.setInterpolationFunctionBooleanWidget.addItem("cosine")
+    self.setInterpolationFunctionBooleanWidget.addItem("welch")
+    self.setInterpolationFunctionBooleanWidget.addItem("lanczos")
+    self.setInterpolationFunctionBooleanWidget.addItem("blackman")
+    self.setInterpolationFunctionBooleanWidget.setToolTip(
+      "Interpolation functions.")
+    parametersImageResamplingLayout.addRow("Interpolation ", self.setInterpolationFunctionBooleanWidget)
+
+    #
+    # Noise Attenuation Parameters Area
+    #
+    parametersNoiseAttenuationCollapsibleButton = ctk.ctkCollapsibleButton()
+    parametersNoiseAttenuationCollapsibleButton.text = "Noise Attenuation Parameters"
+    self.layout.addWidget(parametersNoiseAttenuationCollapsibleButton)
+
+    # Layout within the dummy collapsible button
+    parametersNoiseAttenuationLayout = qt.QFormLayout(parametersNoiseAttenuationCollapsibleButton)
+
+    #
+    # Apply Automatic Conductance Function
+    #
+    self.setApplyAutoConductanceBooleanWidget = ctk.ctkCheckBox()
+    self.setApplyAutoConductanceBooleanWidget.setChecked(True)
+    self.setApplyAutoConductanceBooleanWidget.setToolTip(
+      "Apply an automatic conductance function in order to adjust the conductance parameters using the input image as source. This is recommended because the tissues borders are usually difficult to be empirically infered. If this option is checked, the manual conductance adjustment will be neglected.")
+    parametersNoiseAttenuationLayout.addRow("Apply Automatic Conductance Function", self.setApplyAutoConductanceBooleanWidget)
+
+    #
+    # Auto Conductance Functions
+    #
+    self.setConductanceFunctionBooleanWidget = ctk.ctkComboBox()
+    self.setConductanceFunctionBooleanWidget.addItem("Canny")
+    self.setConductanceFunctionBooleanWidget.addItem("MAD")
+    self.setConductanceFunctionBooleanWidget.addItem("Morphological")
+    self.setConductanceFunctionBooleanWidget.setToolTip(
+      "Automatic conductance functions.")
+    parametersNoiseAttenuationLayout.addRow("Conductance Function ", self.setConductanceFunctionBooleanWidget)
+
+    #
+    # Apply Conductance Regularization
+    #
+    self.setApplyConductanceRegularizationBooleanWidget = ctk.ctkCheckBox()
+    self.setApplyConductanceRegularizationBooleanWidget.setChecked(True)
+    self.setApplyConductanceRegularizationBooleanWidget.setToolTip(
+      "Apply an regularization level to the automatic conductance value obtained above. This is recommended only if the automatic conductance function is overestimating the edge cut level, which may return undesired results (tissue border deterioration). This factor should be interpreted as a reduction level for less conservative edge preservation. This option will be applyed only if the automatic conductance choice is used.")
+    parametersNoiseAttenuationLayout.addRow("Apply Conductance Regularization", self.setApplyConductanceRegularizationBooleanWidget)
+
+    #
+    # Conductance Regularization Value
+    #
+    self.setFilteringCondutanceWidget = ctk.ctkSliderWidget()
+    self.setFilteringCondutanceWidget.maximum = 5
+    self.setFilteringCondutanceWidget.minimum = 1
+    self.setFilteringCondutanceWidget.value = 2
+    self.setFilteringCondutanceWidget.singleStep = 1
+    self.setFilteringCondutanceWidget.setToolTip("Conductance regularization.")
+    parametersNoiseAttenuationLayout.addRow("Conductance Regularization", self.setFilteringCondutanceWidget)
+
+    #
+    # Filtering Parameters: Condutance TODO Adicionar um separador para nao ficar muito junto dos outros paramatros...talvez pular uma linha ou somente colocar um subtitulo "Manual Conductance Adjustment"
+    #
+    self.setFilteringCondutanceWidget = ctk.ctkSliderWidget()
+    self.setFilteringCondutanceWidget.maximum = 50
+    self.setFilteringCondutanceWidget.minimum = 0
+    self.setFilteringCondutanceWidget.value = 10
+    self.setFilteringCondutanceWidget.singleStep = 0.1
+    self.setFilteringCondutanceWidget.setToolTip("Conductance parameter.")
+    parametersNoiseAttenuationLayout.addRow("Conductance ", self.setFilteringCondutanceWidget)
+
+    #
+    # Filtering Parameters: Number of iterations
+    #
+    self.setFilteringNumberOfIterationWidget = ctk.ctkSliderWidget()
+    self.setFilteringNumberOfIterationWidget.maximum = 50
+    self.setFilteringNumberOfIterationWidget.minimum = 1
+    self.setFilteringNumberOfIterationWidget.value = 25
+    self.setFilteringNumberOfIterationWidget.singleStep = 1
+    self.setFilteringNumberOfIterationWidget.setToolTip("Number of iterations parameter.")
+    parametersNoiseAttenuationLayout.addRow("Number Of Iterations ", self.setFilteringNumberOfIterationWidget)
+
+    #
+    # Filtering Parameters: Q value
+    #
+    self.setFilteringQWidget = ctk.ctkSliderWidget()
+    self.setFilteringQWidget.singleStep = 0.01
+    self.setFilteringQWidget.minimum = 0.01
+    self.setFilteringQWidget.maximum = 1.99
+    self.setFilteringQWidget.value = 1.4
+    self.setFilteringQWidget.setToolTip("Q value parameter.")
+    parametersNoiseAttenuationLayout.addRow("Q Value ", self.setFilteringQWidget)
+
+
+    #
+    # Bias Field Correction Parameters Area
+    #
+    parametersBiasCorrectionCollapsibleButton = ctk.ctkCollapsibleButton()
+    parametersBiasCorrectionCollapsibleButton.text = "Bias Field Correction Parameters"
+    self.layout.addWidget(parametersBiasCorrectionCollapsibleButton)
+
+    # Layout within the dummy collapsible button
+    parametersBiasCorrectionLayout = qt.QFormLayout(parametersBiasCorrectionCollapsibleButton)
+
+    #
+    # input mask selector
+    #
+    self.inputMaskSelector = slicer.qMRMLNodeComboBox()
+    self.inputMaskSelector.nodeTypes = ["vtkMRMLLabelMapVolumeNode"]
+    self.inputMaskSelector.selectNodeUponCreation = True
+    self.inputMaskSelector.addEnabled = False
+    self.inputMaskSelector.removeEnabled = True
+    self.inputMaskSelector.noneEnabled = True
+    self.inputMaskSelector.showHidden = False
+    self.inputMaskSelector.showChildNodeTypes = False
+    self.inputMaskSelector.setMRMLScene( slicer.mrmlScene )
+    self.inputMaskSelector.setToolTip( "Pick the input brain mask. This image mask should informs the region of interest where the bias field correction will be applied. If not provided, an Otsu segmentation is performed." )
+    parametersBiasCorrectionLayout.addRow("Image Mask: ", self.inputMaskSelector)
+
+    # TODO Colocar um entrada vector float para o N4ITK -- duas entradas para 1 e 2 level
+
+    #
+    # Global Contrast Enhancement Parameters Area
+    #
+    parametersGlobalContrastEnhancementCollapsibleButton = ctk.ctkCollapsibleButton()
+    parametersGlobalContrastEnhancementCollapsibleButton.text = "Global Contrast Enhancement Parameters"
+    self.layout.addWidget(parametersGlobalContrastEnhancementCollapsibleButton)
+
+    # Layout within the dummy collapsible button
+    parametersGlobalContrastEnhancementLayout = qt.QFormLayout(parametersGlobalContrastEnhancementCollapsibleButton)
+
+    #
+    # Contrast Modulation Functions TODO Montar metodos CLI para cada uma destas funcoes...fica interessante deixar estes independentes para usar em outros metodos no SLicer
+    #
+    self.setContrastModulationFunctionBooleanWidget = ctk.ctkComboBox()
+    self.setContrastModulationFunctionBooleanWidget.addItem("Logistic")
+    self.setContrastModulationFunctionBooleanWidget.addItem("CLAHE") #TODO Implementar CLAHE e CDF image contrast enhancement functions para ITK!!!
+    self.setContrastModulationFunctionBooleanWidget.addItem("CDF")
+    self.setContrastModulationFunctionBooleanWidget.setToolTip(
+      "Contrast modulation functions used to enhance tissue signal contrast in all image scale space. These methods are based on image histogram.")
+    parametersGlobalContrastEnhancementLayout.addRow("Contrast Modulation Function ", self.setContrastModulationFunctionBooleanWidget)
 
     #
     # threshold value

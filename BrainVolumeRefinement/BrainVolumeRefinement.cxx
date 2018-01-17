045c90eb48d8d37c7fcecfc4b926f79fd1dc98d9
Antonio Carlos da Silva Senra Filho <acsenrafilho@gmail.com>
2018-01-17 16:23:12 -0200

    ENH: Brain Volume Refinement module upgrade
    The BVR module is functional, offering a precisa volume correction.

--- BrainVolumeRefinement/BrainVolumeRefinement.cxx
+++ BrainVolumeRefinement/BrainVolumeRefinement.cxx
@@ -1,10 +1,16 @@
 #include "itkImageFileWriter.h"
 
-#include "itkBinaryThresholdImageFilter.h".h"
+#include "itkBinaryThresholdImageFilter.h"
+#include "itkBinaryThinningImageFilter.h"
 #include "itkBinaryContourImageFilter.h"
 #include "itkImageRegionIterator.h"
 #include "itkNeighborhoodIterator.h"
+#include "itkConstantBoundaryCondition.h"
+#include "itkConstNeighborhoodIterator.h"
 #include "itkGradientMagnitudeImageFilter.h"
+#include "itkMedianImageFilter.h"
+#include "itkMaskImageFilter.h"
+#include "itkVotingBinaryIterativeHoleFillingImageFilter.h"
 
 #include "itkPluginUtilities.h"
 
@@ -24,145 +30,181 @@ int DoIt( int argc, char * argv[], TPixel )
     PARSE_ARGS;
 
     typedef TPixel InputPixelType;
-    typedef TPixel OutputPixelType;
     typedef unsigned char LabelPixelType;
 
     const unsigned int Dimension = 3;
 
     typedef itk::Image<InputPixelType,  Dimension>    InputImageType;
-    typedef itk::Image<OutputPixelType, Dimension>    OutputImageType;
     typedef itk::Image<LabelPixelType, Dimension>     LabelImageType;
 
     typedef itk::ImageFileReader<InputImageType>  ImageReaderType;
-    typedef itk::ImageFileReader<LabelImageType>  LabelReaderType;
 
     typename ImageReaderType::Pointer inputReader = ImageReaderType::New();
-    typename LabelReaderType::Pointer maskReader = LabelReaderType::New();
 
     inputReader->SetFileName( inputVolume.c_str() );
+    inputReader->Update();
 
     typedef itk::BinaryThresholdImageFilter<InputImageType, LabelImageType> BinaryThresholdType;
     typename BinaryThresholdType::Pointer estimateMask = BinaryThresholdType::New();
-    if (maskVolume.c_str()=="") {
-        //Making mask of the input volume
-        estimateMask->SetInput(inputReader->GetOutput());
-        estimateMask->SetLowerThreshold(1.0);
-        estimateMask->SetUpperThreshold(itk::NumericTraits<InputPixelType>::max());
-        estimateMask->SetInsideValue(foregroundValue);
-    }else{
-        maskReader->SetFileName( maskVolume.c_str() );
-    }
+
+    //Making mask of the input volume
+    std::cout<<"Making mask of the input volume..."<<std::endl;
+    estimateMask->SetInput(inputReader->GetOutput());
+    estimateMask->SetLowerThreshold(1.0);
+    estimateMask->SetUpperThreshold(itk::NumericTraits<InputPixelType>::max());
+    estimateMask->SetInsideValue(foregroundValue);
 
     // Create volume contour from the input mask
+    std::cout<<"Creating volume contour from the input mask..."<<std::endl;
     typedef itk::BinaryContourImageFilter<LabelImageType, LabelImageType>   BinaryContourType;
     typename BinaryContourType::Pointer maskContour = BinaryContourType::New();
 
-    if (maskVolume.c_str()=="") {
-        maskContour->SetInput(estimateMask->GetOutput());
-    }else{
-        maskContour->SetInput( maskReader->GetOutput() );
-    }
+    maskContour->SetInput(estimateMask->GetOutput());
     maskContour->SetForegroundValue( foregroundValue );
+    maskContour->Update();
+
+        typedef itk::BinaryThinningImageFilter<LabelImageType, LabelImageType>  BinaryThinningType;
+    typename BinaryThinningType::Pointer skeleton = BinaryThinningType::New();
+    skeleton->SetInput(maskContour->GetOutput());
+    skeleton->Update();
 
     // Run through the image gradient (using the countour) in order to find its boundaries
+    std::cout<<"Running through the image gradient (using the countour) in order to find its boundaries..."<<std::endl;
     typedef itk::GradientMagnitudeImageFilter<InputImageType, InputImageType> GradientMagnitudeType;
     typename GradientMagnitudeType::Pointer inputGrad = GradientMagnitudeType::New();
     inputGrad->SetInput(inputReader->GetOutput());
+    inputGrad->Update();
+
+    typename InputImageType::Pointer updatedImage = InputImageType::New();
+    updatedImage->CopyInformation(inputReader->GetOutput());
+    updatedImage->SetRegions(inputReader->GetOutput()->GetRequestedRegion());
+    updatedImage->Allocate();
+    updatedImage->FillBuffer(0);
+    itk::ImageRegionIterator<InputImageType>    inIt(inputReader->GetOutput(), inputReader->GetOutput()->GetRequestedRegion());
+    itk::ImageRegionIterator<InputImageType>    upIt(updatedImage, updatedImage->GetRequestedRegion());
+    upIt.GoToBegin();
+    inIt.GoToBegin();
+    while (!upIt.IsAtEnd()) {
+        upIt.Set(inIt.Get());
+        ++upIt;
+        ++inIt;
+    }
 
     typename LabelImageType::SizeType radius;
     radius[0] = neighborRadius[0];
     radius[1] = neighborRadius[1];
     radius[2] = neighborRadius[2];
-    itk::NeighborhoodIterator<LabelImageType> contourIt(radius, maskContour->GetOutput(),maskContour->GetOutput()->GetRequestedRegion());
-    itk::NeighborhoodIterator<InputImageType> imageIt(radius, inputReader->GetOutput(),inputReader->GetOutput()->GetRequestedRegion());
-    itk::NeighborhoodIterator<InputImageType> gradIt(radius, inputGrad->GetOutput(),inputGrad->GetOutput()->GetRequestedRegion());
-
-
-    typename InputImageType::Pointer updatedImage = InputImageType::New();
-    updatedImage->CopyInformation(inputReader->GetOutput());
-    updatedImage->FillBuffer(0);
-    itk::ImageRegionIterator<InputImageType>    pointImageIt(inputReader->GetOutput(), inputReader->GetOutput()->GetRequestedRegion());
-    itk::ImageRegionIterator<InputImageType>    pointUpdateIt(updatedImage, updatedImage->GetRequestedRegion());
-
-    pointImageIt.GoToBegin();
-    pointUpdateIt.GoToBegin();
-    while (!pointImageIt.IsAtEnd()) {
-        pointUpdateIt.Set(pointImageIt.Get());
-        ++pointImageIt;
-        ++pointUpdateIt;
-    }
-
-    itk::NeighborhoodIterator<InputImageType> updateIt(radius, updatedImage,updatedImage->GetRequestedRegion());
+    itk::ImageRegionIterator<LabelImageType> contourIt(skeleton->GetOutput(),skeleton->GetOutput()->GetRequestedRegion());
+    itk::NeighborhoodIterator<InputImageType, itk::ConstantBoundaryCondition<InputImageType> > imageIt(radius, inputReader->GetOutput(),inputReader->GetOutput()->GetRequestedRegion());
+    itk::NeighborhoodIterator<InputImageType, itk::ConstantBoundaryCondition<InputImageType> > gradIt(radius, inputGrad->GetOutput(),inputGrad->GetOutput()->GetRequestedRegion());
+    itk::NeighborhoodIterator<InputImageType, itk::ConstantBoundaryCondition<InputImageType> > updateIt(radius, updatedImage,updatedImage->GetRequestedRegion());
 
     // Running over the both image and mask contour in order to find the closest voxel in the brain tissue.
+    std::cout<<"Running over the both image and mask contour in order to find the closest voxel in the brain tissue..."<<std::endl;
     contourIt.GoToBegin();
     imageIt.GoToBegin();
-    updateIt.GoToBegin();
     gradIt.GoToBegin();
-
+    updateIt.GoToBegin();
 
     int N = (neighborRadius[0]*2 + 1)*(neighborRadius[1]*2 + 1)*(neighborRadius[2]*2 + 1);
-    while (!contourIt.IsAtEnd()) {
-        if (contourIt.GetCenterPixel()!=static_cast<LabelPixelType>(0)) {
+    InputPixelType meanGrad, meanIntensity;
+    while (!imageIt.IsAtEnd()) {
+        if (contourIt.Get()!=static_cast<LabelPixelType>(0)) {
             //Calculating statistics into the neighborhood
-            if (approach=="GradientMagnitude") {
-                double mean=0.0;
-                for (int p = 0; p < N; ++p) {
-                    mean += gradIt.GetPixel(p);
-                }
-                mean/=N;
-                //Cutting out voxels that does not belongs to the brain based on the mean of the local gradient
-                for (int p = 0; p < N; ++p) {
-                    if (gradIt.GetPixel(p) < (mean)){
-                        updateIt.SetPixel(p, 0);
-                    }
-                }
-            }else if (approach=="Mean") {
-                double mean=0.0;
-                for (int p = 0; p < N; ++p) {
-                    mean += imageIt.GetPixel(p);
-                }
-                mean/=N;
-                //Cutting out voxels that does not belongs to the brain based on the mean value
-                for (int p = 0; p < N; ++p) {
-                    if (imageIt.GetPixel(p) < (mean)){
+            meanGrad=0.0;
+            meanIntensity=0.0;
+            for (unsigned int p = 0; p < N; p++) {
+                meanGrad += gradIt.GetPixel(p);
+                meanIntensity += imageIt.GetPixel(p);
+            }
+            meanGrad/=N;
+            meanIntensity/=N;
+
+            //Cutting out voxels that does not belongs to the brain based on the mean of the local gradient and mean gray level intensity
+            for (int p = 0; p < N; p++) {
+                if (imageIt.GetPixel(p)!=0) {
+                    if (gradIt.GetPixel(p) < (meanGrad) && imageIt.GetPixel(p) < (meanIntensity)){
                         updateIt.SetPixel(p, 0);
                     }
                 }
             }
 
         }
+
         ++contourIt;
         ++imageIt;
         ++updateIt;
+        ++gradIt;
     }
 
+    //Binaring the updated image
+    std::cout<<"Binaring the updated image..."<<std::endl;
+    typename BinaryThresholdType::Pointer newMask = BinaryThresholdType::New();
+    newMask->SetInput(updatedImage);
+    newMask->SetLowerThreshold(1.0);
+    newMask->SetUpperThreshold(itk::NumericTraits<InputPixelType>::max());
+    newMask->SetInsideValue(1.0);
+
+    //Median filter in the input image
+    typedef itk::MedianImageFilter<LabelImageType, LabelImageType>  MedianFilterType;
+    typename MedianFilterType::Pointer median = MedianFilterType::New();
+    median->SetInput(newMask->GetOutput());
+    MedianFilterType::RadiusType mRadius;
+    mRadius[0] = medianRadius[0];
+    mRadius[1] = medianRadius[1];
+    mRadius[2] = medianRadius[2];
+    median->SetRadius(mRadius);
+
+    //Filling in the holes in the updated brain mask
+    std::cout<<"Filling in the holes in the updated brain mask..."<<std::endl;
+    typedef itk::VotingBinaryIterativeHoleFillingImageFilter< LabelImageType > VotingFillInType;
+     typename VotingFillInType::InputSizeType vRadius;
+
+    //Finding the bigger size of the median filter
+    int r = 0.0;
+    for (int i = 0; i < Dimension; ++i) {
+        if (medianRadius[i]>r) {
+            r=medianRadius[i];
+        }
+    }
+      vRadius.Fill( r );
+     typename VotingFillInType::Pointer fillInHoles = VotingFillInType::New();
+      fillInHoles->SetInput( median->GetOutput() );
+      fillInHoles->SetRadius( vRadius );
+      fillInHoles->SetMajorityThreshold( majorityThreshold );
+      fillInHoles->SetBackgroundValue( 0 );
+      fillInHoles->SetForegroundValue( 1 );
+      fillInHoles->SetMaximumNumberOfIterations( numberOfIterations );
+
+    //Masking the input image with the corrected brain mask
+    std::cout<<"Masking the input image with the corrected brain mask..."<<std::endl;
+    typedef itk::MaskImageFilter<InputImageType, LabelImageType>    MaskFilterType;
+    typename MaskFilterType::Pointer maskInput = MaskFilterType::New();
+    maskInput->SetInput(inputReader->GetOutput());
+    maskInput->SetMaskImage(fillInHoles->GetOutput());
+    maskInput->Update();
+
     // Output updated image with less non-brain tissues
+    std::cout<<"Output updated image with less non-brain tissues at location: "<<updatedVolume.c_str()<<std::endl;
     typedef itk::ImageFileWriter<InputImageType> WriterType;
-    typename WriterType::Pointer writer = WriterType::New();
-    writer->SetFileName( updatedVolume.c_str() );
-    writer->SetInput( updatedImage );
-    writer->SetUseCompression(1);
-    writer->Update();
+    typename WriterType::Pointer imageWriter = WriterType::New();
+    imageWriter->SetFileName( updatedVolume.c_str() );
+    imageWriter->SetInput( maskInput->GetOutput() );
+    imageWriter->SetUseCompression(1);
+    imageWriter->Update();
+
+    if (updatedMask!="") {
+        std::cout<<"Output updated mask with less non-brain tissues at location: "<<updatedMask.c_str()<<std::endl;
+        typedef itk::ImageFileWriter<LabelImageType> WriterType;
+        typename WriterType::Pointer labelWriter = WriterType::New();
+        labelWriter->SetFileName( updatedMask.c_str() );
+        labelWriter->SetInput( median->GetOutput() );
+        labelWriter->SetUseCompression(1);
+        labelWriter->Update();
+    }
 
     return EXIT_SUCCESS;
 }
-
-//template <typename TPixel>
-//void meanNeighbors(typename NeighborhoodIterator neighbors, double mean, double std){
-//    N=(neighbors.GetRadius()[0]*2 + 1)*(neighbors.GetRadius()[1]*2 + 1)*(neighbors.GetRadius()[2]*2 + 1);
-//    mean=0.0;
-//    for (int p = 0; p < N; ++p) {
-//        mean += neighbors.GetPixel(p);
-//    }
-//    mean/=N;
-
-//    std=0.0;
-//    for (int p = 0; p < N; ++p) {
-//        std += std::pow<double>(mean - neighbors.GetPixel(p),2.0);
-//    }
-//    std/= std::sqrt(std/(N-1));
-//}
 
 } // end of anonymous namespace
 
